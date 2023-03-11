377772baf8c2ce670ded2f5cb1738cdcf5089c7b
ssstoyama <ssstoyama.develop@gmail.com>
2023-03-11 14:58:07 +0000

    add clear command

--- shell.zig
+++ shell.zig
@@ -1,5 +1,6 @@
 const uefi = @import("std").os.uefi;
 const common = @import("common.zig");
+const ST = @import("efi.zig").ST;
 
 const MAX_COMMAND_LEN: usize = 100;
 
@@ -7,13 +8,15 @@ pub fn run() void {
     var command: [MAX_COMMAND_LEN]u16 = .{0} ** MAX_COMMAND_LEN;
 
     while (true) {
-        common.puts(&[_:0]u16{ 'u', 'e', 'f', 'i', '>', ' ' });
+        common.puts("uefi>");
         if (common.gets(@ptrCast([*:0]u16, &command), MAX_COMMAND_LEN) <= 0) continue;
 
         if (common.strcmp(&[_:0]u16{ 'h', 'e', 'l', 'l', 'o' }, @ptrCast([*:0]u16, &command)) == 0) {
-            common.puts(&[_:0]u16{ 'H', 'e', 'l', 'l', 'o', ' ', 'U', 'E', 'F', 'I', '!', '\r', '\n' });
+            common.puts("Hello UEFI!\r\n");
+        } else if (common.strcmp(&[_:0]u16{ 'c', 'l', 'e', 'a', 'r' }, @ptrCast([*:0]u16, &command)) == 0) {
+            _ = ST().con_out.?.clearScreen();
         } else {
-            common.puts(&[_:0]u16{ 'C', 'o', 'm', 'm', 'a', 'n', 'd', ' ', 'n', 'o', 't', ' ', 'f', 'o', 'u', 'n', 'd', '.', '\r', '\n' });
+            common.puts("Command not found\r\n");
         }
     }
 }
